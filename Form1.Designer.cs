516dcdf0e1b456d00e3b6c05a02864422ddfe4e7
simonguenther <simonpublic@fam-guenther.com>
2017-11-05 10:10:40 +0100

    Main Form

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PokerMasterSuite2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnLayoutEditor = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(93, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLayoutEditor
+            // 
+            this.btnLayoutEditor.Location = new System.Drawing.Point(78, 79);
+            this.btnLayoutEditor.Name = "btnLayoutEditor";
+            this.btnLayoutEditor.Size = new System.Drawing.Size(105, 23);
+            this.btnLayoutEditor.TabIndex = 1;
+            this.btnLayoutEditor.Text = "Layout Editor";
+            this.btnLayoutEditor.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(93, 50);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 136);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnLayoutEditor);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PokerMasterSuite2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnLayoutEditor;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokerMasterSuite2
+{
+    public partial class Form1 : Form
+    {
+        bool isRunning = false;
+        static SessionHandler sessionHandler = new SessionHandler();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //sessionHandler.attachScanControls();
+            btnRefresh.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(isRunning) // Click => Stop => Idle
+            {
+                // Check for running seating scripts
+                sessionHandler.detachScanControls();
+                btnStart.Text = "Start";
+                isRunning = false;
+                btnRefresh.Visible = false;
+            }
+            else // Click => Start => Running
+            {
+                sessionHandler.attachScanControls();
+                btnStart.Text = "Stop";
+                isRunning = true;
+                btnRefresh.Visible = true;
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            sessionHandler.attachScanControls();
+        }
+    }
+}
