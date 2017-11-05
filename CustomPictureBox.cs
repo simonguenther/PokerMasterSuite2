559ca2275b63233ef1b5d4c7731b174bcc23bcd5
simonguenther <simonpublic@fam-guenther.com>
2017-11-05 10:08:52 +0100

    Extends Picture Box for locking and double retrieves

---
+++ CustomPictureBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokerMasterSuite2
+{
+    /// <summary>
+    /// Extends PictureBox for storing TableHandle, SimilarNickList and PlayerButton references
+    /// </summary>
+    public class CustomPictureBox : PictureBox
+    {
+        /// <summary>
+        /// Stores pointer of table the picturebox is attached to
+        /// </summary>
+        public IntPtr TableHandle { get; private set; }
+
+        /// <summary>
+        /// Stores String-List of SmilarNicks which then are going to get displayed if someone clicks on the picturebox
+        /// </summary>
+        public List<Note> SimilarNicks { get; set; }
+
+        /// <summary>
+        /// PlayerButton the picturebox relates to
+        /// </summary>
+        public PlayerButton PlayerButton { get; set; }
+
+        /// <summary>
+        /// Setting Parent for easier referencing later on
+        /// </summary>
+        /// <param name="newParent"></param>
+        public void SetParent(IntPtr newParent)
+        {
+            this.TableHandle = newParent;
+            if (!TableHandle.Equals(IntPtr.Zero))
+            {
+                WinAPI.SetParent(this.Handle, TableHandle);
+            }
+        }
+    }
+}
