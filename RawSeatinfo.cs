f6cdae4a433574b92233e9171db6569d5c220566
simonguenther <simonpublic@fam-guenther.com>
2017-11-05 10:18:57 +0100

    Helper Class for handling images before and after OCR

---
+++ RawSeatinfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerMasterSuite2
+{
+    /// <summary>
+    /// Helper Class for handling images before and after OCR
+    /// </summary>
+    class RawSeatInfo
+    {
+        public RawSeatInfo(Bitmap nick, Bitmap ava)
+        {
+            nicknameBMP = nick;
+            avatar = ava;
+        }
+
+        public System.Drawing.Bitmap nicknameBMP { set; get; }
+        public System.Drawing.Bitmap avatar { set; get; }
+        public string nicknameSTRING { set; get; }
+    }
+
+}
