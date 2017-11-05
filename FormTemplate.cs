2e48f8fb404f847541369eca63ac51ff765b6ade
simonguenther <simonpublic@fam-guenther.com>
2017-11-05 10:12:29 +0100

    Extends Forms. Template with built in functions for all forms

---
+++ FormTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokerMasterSuite2
+{
+    /// <summary>
+    /// Form Template which adds "Close on Escape-Button" to all Forms
+    /// </summary>
+    public class FormTemplate : Form
+    {
+        /// <summary>
+        /// Close on Escape
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="keyData"></param>
+        /// <returns></returns>
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // Close noteWindow discarding changes on Escape
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
