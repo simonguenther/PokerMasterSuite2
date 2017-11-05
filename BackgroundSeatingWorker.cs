0ba6fc98270886c5d26f1603656d159889fdf4a3
simonguenther <simonpublic@fam-guenther.com>
2017-11-05 10:07:25 +0100

    Extends BackgroundWorker for SeatingScript

---
+++ BackgroundSeatingWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerMasterSuite2
+{
+    /// <summary>
+    /// Extension of BackgroundWorker, only adds an option to store ScanButton it belongs to
+    /// </summary>
+    class BackgroundSeatingWorker : BackgroundWorker
+    {
+        public ScanButton scanButton { get; set; }
+    }
+}
